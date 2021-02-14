b41a4161d79d9ed51eccd45e14b4cf9aaafc07bd
Venkata Raman <rsvr000@gmail.com>
2021-02-14 19:56:26 +0530

    added state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
 class CartItem extends React.Component{
-   render(){
+   constructor(){
+       super();
+    this.state={
+        price:999,
+        title:'Mobile Phone',
+        qty:1,
+        img:''
+    }
+   }
+    render(){
+        const{price,title,qty}=this.state;
        return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 99</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs. {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*button*/}
+                        <img alt="increase" className="action-icons" src="https://as1.ftcdn.net/jpg/02/51/03/82/500_F_251038282_CLb3d8tk99bGoU9ILEYS8vY215fgRmGT.jpg"/>
+                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"/>
+                        <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/jpg/03/73/50/09/500_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg"/> 
                     </div>
                 </div>
             </div>
